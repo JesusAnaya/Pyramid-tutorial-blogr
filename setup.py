0fa6e4d467ff61e8d6fa4c6816c05cf4a507e10b
Jesus Anaya <jesus.anaya.dev@gmail.com>
2016-03-02 17:24:04 -0800

    first commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+with open(os.path.join(here, 'requirements.txt')) as f:
+    requires = f.read().splitlines()
+
+setup(name='pyramid_blogr',
+      version='0.0.1',
+      description='pyramid_blogr',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='pyramid_blogr',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = pyramid_blogr:main
+      [console_scripts]
+      initialize_pyramid_blogr_db = pyramid_blogr.scripts.initializedb:main
+      """,
+      )
