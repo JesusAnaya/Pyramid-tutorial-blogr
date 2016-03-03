bf14abb179b259352ea6d7a752fd2ec770c74aff
Jesus Anaya <jesus.anaya.dev@gmail.com>
2016-03-02 19:02:00 -0800

    Added first templates for view and edit blog posts

--- pyramid_blogr/views/default.py
+++ pyramid_blogr/views/default.py
@@ -1,9 +1,12 @@
 from pyramid.view import view_config
+from ..models.services.blog_record import BlogRecordService
 
 
 @view_config(route_name='home', renderer='pyramid_blogr:templates/index.jinja2')
 def index_page(request):
-    return {}
+    page = int(request.params.get('page', 1))
+    paginator = BlogRecordService.get_paginator(request, page)
+    return {'paginator': paginator}
 
 
 @view_config(route_name='auth', match_param='action=in', renderer='string', request_method='POST')
