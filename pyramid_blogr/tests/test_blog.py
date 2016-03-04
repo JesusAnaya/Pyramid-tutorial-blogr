fe7937b2776f3436b9abe02e8ae19e57a7c209e7
Jesus Anaya <jesus.anaya.dev@gmail.com>
2016-03-03 19:18:11 -0800

    Fixed small issue in datetime format

--- pyramid_blogr/tests/test_blog.py
+++ pyramid_blogr/tests/test_blog.py
@@ -11,7 +11,7 @@ class TestBlogModelMethods(TestCase):
 
     def test_blog_post_created_in_words(self):
         datetime_patched = self.add_patch('pyramid_blogr.models.blog_record.datetime')
-        datetime_patched.datetime.utcnow.return_value = datetime.datetime(2016, 02, 10)
+        datetime_patched.datetime.utcnow.return_value = datetime.datetime(2016, 2, 10)
 
         blog_record = factories.BlogRecordFactory.create(created=datetime.datetime(2015, 10, 10))
 
