67b2bbb80b52337dddd6f106f5a83114a079ddc4
Jesus Anaya <jesus.anaya.dev@gmail.com>
2016-03-03 16:25:41 -0800

    Add autthentication and authorization policies to blog view for create and update a record

--- pyramid_blogr/__init__.py
+++ pyramid_blogr/__init__.py
@@ -1,4 +1,6 @@
 from pyramid.config import Configurator
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
 from sqlalchemy import engine_from_config
 
 from .models.meta import (
@@ -13,12 +15,14 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
-    config = Configurator(settings=settings)
+    authentication_policy = AuthTktAuthenticationPolicy('somesecret', hashalg='sha512')
+    authorization_policy = ACLAuthorizationPolicy()
+    config = Configurator(settings=settings, authentication_policy=authentication_policy, authorization_policy=authorization_policy)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('blog', '/blog/{id:\d+}/{slug}')
-    config.add_route('blog_action', '/blog/{action}')
+    config.add_route('blog_action', '/blog/{action}', factory='pyramid_blogr.security.BlogRecordFactory')
     config.add_route('auth', '/sign/{action}')
     config.scan()
     return config.make_wsgi_app()
