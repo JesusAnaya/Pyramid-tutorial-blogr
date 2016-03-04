0565a57888c2c315509e485898136d658e7b74b9
Jesus Anaya <jesus.anaya.dev@gmail.com>
2016-03-03 16:36:58 -0800

    Add authenticaion from and methods for manage that authentication in a session context

--- pyramid_blogr/views/default.py
+++ pyramid_blogr/views/default.py
@@ -1,4 +1,7 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+from pyramid.security import remember, forget
+from ..models.services.user import UserService
 from ..models.services.blog_record import BlogRecordService
 
 
@@ -12,4 +15,13 @@ def index_page(request):
 @view_config(route_name='auth', match_param='action=in', renderer='string', request_method='POST')
 @view_config(route_name='auth', match_param='action=out', renderer='string')
 def sign_in_out(request):
-    return {}
+    username = request.POST.get('username')
+    if username:
+        user = UserService.by_name(username)
+        if user and user.verify_password(request.POST.get('password')):
+            headers = remember(request, user.name)
+        else:
+            headers = forget(request)
+    else:
+        headers = forget(request)
+    return HTTPFound(location=request.route_url('home'), headers=headers)
